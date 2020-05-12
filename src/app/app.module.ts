232cdf171ea79f2723c7d7db3dfe87ff0b265b4e
unknown <cristian.dejica00@gmail.com>
2020-05-12 15:30:21 +0300

    Images to png

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
