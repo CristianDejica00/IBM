3a56610f5e632fbabccd5e8f47a63d642bc67952
user.name <andrei.guta99@e-uvt.ro>
2020-05-14 11:03:29 +0300

    added services

---
+++ src/app/shared/models/certificate.ts
@@ -0,0 +1,8 @@
+export class Certificate {
+  id: string;
+  title: string;
+  quarter: string;
+  category: string;
+  cost: number;
+  description: string;
+}
---
+++ src/app/shared/models/request.ts
@@ -0,0 +1,10 @@
+export class Request {
+    id: string;
+    price: number;
+    userName: string;
+    certificationTitle: string;
+    status: string;
+    certificateQuarter: string;
+    justificationText: string;
+  }
+  
